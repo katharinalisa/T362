6e3fa49fabe61edc41311564e9d89715b96bd1b8
Tram Nguyen <ngnbtraam@gmail.com>
2025-06-12 23:41:54 +1000

    Update main.py

--- PrimeTime/main.py
+++ PrimeTime/main.py
@@ -1,5 +1,6 @@
-from website import create_app
+from primetime_toolkit import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
