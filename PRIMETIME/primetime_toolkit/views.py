ffd3bef6a9303b71b8cf5f4d75815a44dbe432e5
Tram Nguyen <ngnbtraam@gmail.com>
2025-06-13 16:56:27 +1000

    Update views.py

--- PRIMETIME/primetime_toolkit/views.py
+++ PRIMETIME/primetime_toolkit/views.py
@@ -5,3 +5,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template('index.html')
+
+@views.route('/budget')
+def budget():
+    return render_template('budget.html')
