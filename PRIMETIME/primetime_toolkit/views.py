b13dedcee0340b5430ef8477e08eef5c70941954
Tram Nguyen <ngnbtraam@gmail.com>
2025-08-05 10:06:07 +1000

    Update views.py

--- PRIMETIME/primetime_toolkit/views.py
+++ PRIMETIME/primetime_toolkit/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Assessment
+from . import db
 
 views = Blueprint('views', __name__)
 print("__name__ : ",__name__)
@@ -15,11 +17,52 @@ def budget():
 def category():
     return render_template('category.html')
 
+<<<<<<< HEAD
 @views.route('/category/financial')
 def financial():
     return render_template('/diagnostic/financial.html')
+=======
+@views.route('/self-assessment' , methods=['GET'])
+def assessment():
+    return render_template('assessment.html')
+>>>>>>> f5299313a038d289890686694e2a9375109334ad
 
 @views.route('/category/careertransition')
 def financial():
     return render_template('/diagnostic/careertransition.html')
 
+<<<<<<< HEAD
+=======
+@views.route('/submit-assessment', methods=['POST'])
+def submit_assessment():
+    # q1, q2... etc. are the responses (yes/no) of users
+    q1 = request.form.get('q1')
+    q2 = request.form.get('q2')
+    q3 = request.form.get('q3')
+    q4 = request.form.get('q4')
+    q5 = request.form.get('q5')
+    q6 = request.form.get('q6')
+    q7 = request.form.get('q7')
+
+    # This creates a new row in the Assessments table for a new user
+    new_response = Assessment(
+        q1=q1,
+        q2=q2,
+        q3=q3,
+        q4=q4,
+        q5=q5,
+        q6=q6,
+        q7=q7
+    )
+
+    # Adding new changes to database
+    db.session.add(new_response)
+    db.session.commit()
+
+    #After user submits the form, he will be redirected to views.py
+    return redirect(url_for('views.summary'))
+
+@views.route('/summary-page')
+def summary():
+    return "Form submitted!"
+>>>>>>> f5299313a038d289890686694e2a9375109334ad
