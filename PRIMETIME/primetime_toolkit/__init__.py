01db716234a8c9cfad49bda8ed9f96a7381c4998
Tram Nguyen <ngnbtraam@gmail.com>
2025-08-05 10:05:54 +1000

    Update __init__.py

--- PRIMETIME/primetime_toolkit/__init__.py
+++ PRIMETIME/primetime_toolkit/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy() 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key'
+    app.config['SECRET_KEY'] = 'our-secret'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///primetime.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
 
     from .auth import auth
     from .views import views
