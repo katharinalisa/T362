47bcfb48e52a01d7e3e2b01660a036f0d5e2e372
Tram Nguyen <ngnbtraam@gmail.com>
2025-08-05 14:15:55 +1000

    Update __init__.py

--- PRIMETIME/primetime_toolkit/__init__.py
+++ PRIMETIME/primetime_toolkit/__init__.py
@@ -11,10 +11,10 @@ def create_app():
 
     db.init_app(app)
 
-    from .auth import auth
+    # from .auth import auth
     from .views import views
     
-    app.register_blueprint(auth, url_prefix='/auth')
-    app.register_blueprint(views, url_prefix='/')
+    # app.register_blueprint(auth, url_prefix='/auth')
+    # app.register_blueprint(views, url_prefix='/')
 
     return app
