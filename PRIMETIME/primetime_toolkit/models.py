f9a5af2aad5dd96bea572c243bc3126a41776e51
Tram Nguyen <ngnbtraam@gmail.com>
2025-08-05 14:48:18 +1000

    Update models.py

--- PRIMETIME/primetime_toolkit/models.py
+++ PRIMETIME/primetime_toolkit/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from . import db
 
 class Assessment(db.Model):
@@ -10,23 +9,3 @@ class Assessment(db.Model):
     q5 = db.Column(db.String(10))
     q6 = db.Column(db.String(10))
     q7 = db.Column(db.String(10))
-
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    FirstName = db.Column(db.String(35))
-    LastName = db.Column(db.String(35))
-    Age = db.Column(db.Integer)
-    Address = db.Column(db.String(90))
-=======
-from . import db
-
-class Assessment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    q1 = db.Column(db.String(10))
-    q2 = db.Column(db.String(10))
-    q3 = db.Column(db.String(10))
-    q4 = db.Column(db.String(10))
-    q5 = db.Column(db.String(10))
-    q6 = db.Column(db.String(10))
-    q7 = db.Column(db.String(10))
->>>>>>> f5299313a038d289890686694e2a9375109334ad
