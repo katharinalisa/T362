81e0814d61811e3e9e548003581ae32a9f2e283e
katharinalisa <ka.gremer@gmail.com>
2025-08-10 16:43:57 +1000

    Just a few changes

--- PRIMETIME/primetime_toolkit/models.py
+++ PRIMETIME/primetime_toolkit/models.py
@@ -2,10 +2,20 @@ from . import db
 
 class Assessment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    q1 = db.Column(db.Integer)
-    q2 = db.Column(db.Integer)
-    q3 = db.Column(db.Integer)
-    q4 = db.Column(db.Integer)
-    q5 = db.Column(db.Integer)
-    q6 = db.Column(db.Integer)
-    q7 = db.Column(db.Integer)
+    q1 = db.Column(db.Integer, nullable=False)
+    q2 = db.Column(db.Integer, nullable=False)
+    q3 = db.Column(db.Integer, nullable=False)
+    q4 = db.Column(db.Integer, nullable=False)
+    q5 = db.Column(db.Integer, nullable=False)
+    q6 = db.Column(db.Integer, nullable=False)
+    q7 = db.Column(db.Integer, nullable=False)
+
+
+    def __init__(self, q1, q2, q3, q4, q5, q6, q7):
+        self.q1 = q1
+        self.q2 = q2
+        self.q3 = q3
+        self.q4 = q4
+        self.q5 = q5
+        self.q6 = q6
+        self.q7 = q7
--- PRIMETIME/primetime_toolkit/views.py
+++ PRIMETIME/primetime_toolkit/views.py
@@ -20,7 +20,6 @@ def assessment():
 
 @views.route('/submit-assessment', methods=['POST'])
 def submit_assessment():
-    # q1, q2... etc. are the responses (yes/no) of users
     q1 = int(request.form.get('q1'))
     q2 = int(request.form.get('q2'))
     q3 = int(request.form.get('q3'))
@@ -29,7 +28,6 @@ def submit_assessment():
     q6 = int(request.form.get('q6'))
     q7 = int(request.form.get('q7'))
 
-    # This creates a new row in the Assessments table for a new user
     new_response = Assessment(
         q1=q1,
         q2=q2,
@@ -40,15 +38,13 @@ def submit_assessment():
         q7=q7
     )
 
-    # Adding new changes to database
     db.session.add(new_response)
     db.session.commit()
 
-    # Calculate the total score
+
     total_score = q1 + q2 + q3 + q4 + q5 + q6 + q7
 
     def get_result_description(score):
-        # Determine the result based on the total score
         if score >= 30:
             return "Excellent"
         elif score >= 20:
